bdc2db40b966966fb643f3ed4cf0b239f528c5e1
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-27 23:21:27 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World, it's theVTOQ!"
+    resp.write "Hello, my name is theVTOQ!"
     resp.finish
   end
 
